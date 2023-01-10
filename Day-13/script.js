89d8a7ebb217389df0f47e17315127c1d1386d31
Tanishq9TSJ <92173241+Tanishq9TSJ@users.noreply.github.com>
2023-01-10 23:12:05 +0530

    Add files via upload

---
+++ Day-13/script.js
@@ -0,0 +1,17 @@
+const allStars = document.querySelectorAll('.star');
+allStars.forEach((star, i) =>{
+    star.onclick = function(){
+        console.log(star);
+        let current_star_level = i +1;
+        allStars.forEach( (star,j) =>{
+            if(current_star_level >= j+1)
+            {
+                star.innerHTML = '&#9733';
+            }
+            else
+            {
+                star.innerHTML = '&#9734';
+            }
+        })
+    }
+})
